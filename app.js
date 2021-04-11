f1aa26642a35ab37bb42e9ed2e6bcdc271c94190
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-04-11 18:39:54 +0700

    Reactive Data Vue.Js

---
+++ app.js
@@ -0,0 +1,11 @@
+var profile = {
+    "nama": "Ahmat Arif Ilahaka",
+    "ttl": "Semarang, 1 Mei 1999",
+    "pekerjaan": "Programmer (Back-end Developer)",
+    "alamat": "Jalan Karangrejo Utara dalam V no.37a RT.004/RW.007, Srondol Wetan, Banyumanik, Semarang"
+};
+
+var app = new Vue({
+    el: '#app',
+    data: profile
+});
