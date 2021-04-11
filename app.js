456f140aad6c08af476eeae5fceb52ee986408b4
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-04-12 06:20:05 +0700

    melakukan binding di vue.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var profile = {
-    "nama": "Ahmat Arif Ilahaka",
-    "ttl": "Semarang, 1 Mei 1999",
-    "pekerjaan": "Programmer (Back-end Developer)",
-    "alamat": "Jalan Karangrejo Utara dalam V no.37a RT.004/RW.007, Srondol Wetan, Banyumanik, Semarang"
+    "nama": "T-Shirt Gintama",
+    "description": "Baju pria dengan gambar character dari seri Gintama. Ukuran bisa dipilih sesuai keinginan",
+    "price": "Rp 150.0000,00-",
+    "image": "https://i.pinimg.com/originals/d8/22/c3/d822c3041af5cd2b99eda93984cf510d.jpg"
 };
 
 var app = new Vue({
